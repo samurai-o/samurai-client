8f2c410ca0d95b658a1570909dc4def88d9eeb8a
刘伟 <liuwei03@dongfangfuli.com>
2021-07-08 14:57:51 +0800

    fix: loginout api

--- src/common/apis/auth.api.ts
+++ src/common/apis/auth.api.ts
@@ -16,7 +16,7 @@ class Auth extends Network {
     return data;
   }
 
-  @Network.Get('/outlogin')
+  @Network.Get('/api/outlogin')
   outlogin(params?: any, res?: any) {
     const { data } = res;
     if (data) window.location.href = `${window.location.origin}/auth/login`;
