ceadf2c2f57bf92ae161141421f4af8f0709a5f5
刘伟 <liuwei03@dongfangfuli.com>
2021-07-08 20:19:58 +0800

    feat: graphql加入

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { Header } from '@/common/header';
 import logo from '@/assets/logo.png';
 import { CoreLocationProps } from '@/common/interfaces/core.interface';
 import { LayoutBase, LayoutContent } from './index.styled';
 
+const client = new ApolloClient({ uri: '', cache: new InMemoryCache() });
+
 export default function Base(props: CoreLocationProps) {
   const header = useRef<HTMLElement>(null);
-  console.log(props);
   const [style, setStyle] = useState({
     paddingTop: 0,
     minHeight: '100vh',
@@ -18,9 +20,13 @@ export default function Base(props: CoreLocationProps) {
   }, [header.current]);
 
   return (
-    <LayoutBase style={style}>
-      <Header logo={logo} ref={header} />
-      <LayoutContent height={style.paddingTop}>{props.children}</LayoutContent>
-    </LayoutBase>
+    <ApolloProvider client={client}>
+      <LayoutBase style={style}>
+        <Header logo={logo} ref={header} />
+        <LayoutContent height={style.paddingTop}>
+          {props.children}
+        </LayoutContent>
+      </LayoutBase>
+    </ApolloProvider>
   );
 }
